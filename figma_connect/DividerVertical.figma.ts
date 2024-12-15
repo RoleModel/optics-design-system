ff5bdf8f626dd089631a1d7889c6f24a2c09ce6b
Jeremy Walton <jeremy.patrick.walton@gmail.com>
2024-12-15 14:23:30 -0500

    Add divider

---
+++ figma_connect/DividerVertical.figma.ts
@@ -0,0 +1,21 @@
+import figma, { html } from '@figma/code-connect/html';
+
+figma.connect('https://www.figma.com/design/1UlVLD3gS4KUTuQSPRG3bl/Optics---Components?m=auto&node-id=7534-7819&t=ZPXbcTbeL1e5Hs7x-1', {
+  props: {
+    className: figma.className([
+      'divider-vertical',
+      figma.enum('Size', {
+        '1px': 'divider--small',
+        '2px': 'divider--medium',
+        '4px': 'divider--large'
+      }),
+      figma.enum('Spacing', {
+        '0px': 'divider--spacing-none',
+        '4px': 'divider--spacing-small',
+        '8px': 'divider--spacing-medium',
+        '16px': 'divider--spacing-large'
+      })
+    ])
+  },
+  example: ({ className }) => html`<div class=${className}></div>`
+})
